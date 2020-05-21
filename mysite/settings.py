dcd7b158f6da62874f9d8c93bfd4cef371599e8a
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-05-20 21:25:37 -0400

    removed lines for security

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't4)p0u%0_pti_&c0^ppfqygkhm4@(lak-@j+)u3fej0ta=@6s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

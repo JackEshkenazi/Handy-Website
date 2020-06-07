7777508ef3f30274d7e9623c7a2083747d900402
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-06-07 00:43:27 -0400

    on track to figuring out backend query functionality

--- handy/models.py
+++ handy/models.py
@@ -22,7 +22,7 @@ class Contractor(models.Model):
     
     def __str__(self):
         """String for representing the Model object."""
-        return self.name
+        return str(self.name) + " " + str(self.city.name)
     
     def get_absolute_url(self):
         """Returns the url to access a detail record for this contractor."""
--- handy/views.py
+++ handy/views.py
@@ -1,19 +1,55 @@
+from django.db.models.sql.datastructures import Join
 from django.http import HttpResponse
 from django.shortcuts import render
-from handy.models import Contractor
+from handy.models import Contractor, City
+from django.db.models.functions import Concat
+from django.db.models import Aggregate, CharField, Value
+
+class Card:
+  name: str
+  phone: str
+  email: str
+  cities = []
+
+
+
+class GroupConcat(Aggregate):
+    # supports COUNT(distinct field)
+    function = 'GROUP_CONCAT'
+    template = '%(function)s(%(expression)s, "%(separator)s")'
+
+
+    def __init__(self, expression, separator=',', **extra):
+        super(GroupConcat, self).__init__(
+            expression,
+            separator=separator,
+            output_field=CharField(),
+            **extra
+        )
 
 
 def index(request):
+
+  
+  contractor_ids = Contractor.objects.only("contractor_id")
+
+  cards = dict()
+  query_result = Contractor.objects.annotate(cities=GroupConcat('city',separator=','))
+  for row in query_result:
+    print(row)
+  
     
-  dataContractors = Contractor.objects.all()
+  # contractors = Contractor.objects.all()
+  # cities = Cities.objects.all()
+  # for contractor in contractors:
+  #   contractor_cities = Contractor.city.through.objects.filter(contractor_id = contractor.id).only("city_id")
+  #   contractor.cities = contractor_cities
 
-  dataCities = Contractor.city.through.objects.all()
 
-  #print(data[2])
+  data = Contractor.objects.raw('SELECT hc.id, hc.name, hc.phone, hc.email, hcity.name FROM handy_contractor as hc LEFT JOIN handy_contractor_city as hcc ON hcc.contractor_id = hc.id LEFT JOIN handy_city as hcity ON hcc.city_id = hcity.id;')
 
   context={
-    "data": dataContractors,
-    "cities": dataCities
+    "data": data
   }
 
   return render(request, 'index.html', context)

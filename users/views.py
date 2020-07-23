d6e87af35e288e556a8bf6b51250dcca110e011a
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-07-22 23:31:25 -0400

    checking for valid post requests

--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
+from handy import views as handy_views
 
 # Create your views here.
 
 def register(request):
 
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            print("SOMETHING WORKED")
+            return redirect('index')
+    else:
+        form = UserCreationForm()
 
     context={
         "form": form

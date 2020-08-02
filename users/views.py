c8ab671078596273ef4420ff857351894d567831
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-08-01 23:34:22 -0400

    utilised crispy forms

--- users/views.py
+++ users/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 
 from handy import views as handy_views
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def register(request):
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             print("SOMETHING WORKED")
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     context={
         "form": form

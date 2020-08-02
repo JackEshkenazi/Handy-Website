a009f83dc81873813206eac0a0dad9a58e32889f
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-08-01 23:46:12 -0400

    made the email field into a crispy field that works

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    #email = forms.EmailField()
+    #only doesnt work when its specifically an EmailField
+
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
 
     class Meta:
         model = User

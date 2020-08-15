b1b8bb0ebd476f1781926d57a91523d621839b7d
JackEshkenazi <jack.eshkenazi@gmail.com>
2020-08-14 21:54:37 -0400

    removed reference to login and logout in wrong app

--- handy/urls.py
+++ handy/urls.py
@@ -24,9 +24,11 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
+    '''
     path('login/', auth_views.LoginView.as_view(), name='login'),
 
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    '''
 
     path('contractor/<int:ID>', dynamic_lookup_view, name='dynamic_lookup_view'),
 
